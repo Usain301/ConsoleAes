59fd0ac8673d302ccddc86afe50f96436bd55fc1
Usain301 <73827957+Usain301@users.noreply.github.com>
2021-10-17 17:22:05 +0300

    Add project files.

---
+++ ConsoleAes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleAes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+         string data = System.Console.ReadLine();
+        string encrypted = data.Encrypt();
+        System.Console.WriteLine("Encrypted: " + encrypted);
+            System.Console.WriteLine("Decrypted: " + encrypted.Decrypt());
+            System.Console.ReadKey();
+        }
+}
+}
